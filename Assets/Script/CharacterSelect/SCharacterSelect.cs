ab9df17efb58051af65f88f941d6fd8cfa7c1e7b
KIMSEOYOON1 <khj8260@naver.com>
2022-04-20 20:40:30 +0900

    캐릭터선택

--- Assets/Script/CharacterSelect/SCharacterSelect.cs
+++ Assets/Script/CharacterSelect/SCharacterSelect.cs
@@ -10,35 +10,27 @@ public class SCharacterSelect : MonoBehaviour
     public GameObject button;
     public Vector3 Dir;
     float Rot;
+    bool Move = false;
 
     public void ChangeWomen()
     {
-        
+        Move = true;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    
-        // Update is called once per frame
-     void Update()
-     {
-         transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0.0f, transform.localPosition.y, transform.localPosition.z), Time.deltaTime * 1.0f);
-            if (Rot > Mathf.Epsilon)
-             {
-                float delta = Time.deltaTime * 180.0f;
-                if (delta > Rot)
-                 {
-                    delta = Rot;
-                 }
-                Rot -= delta;
-                transform.Rotate(Vector3.up * delta);
-             }
-           
-     }
-   
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Move == true)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0.0f, transform.localPosition.y, transform.localPosition.z), Time.deltaTime * 1.0f);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 170, 0), Time.deltaTime * 5.0f);
+        }
+    }
 }
 
