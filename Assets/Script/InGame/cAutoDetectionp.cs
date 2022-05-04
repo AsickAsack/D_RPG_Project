8d5acf59b1ec8030c981cb959bc06eff95b69e9a
PPARKSHAUN <92656701+PPARKSHAUN@users.noreply.github.com>
2022-05-04 20:14:59 +0900

    몬스터 공격추가

---
+++ Assets/Script/InGame/cAutoDetectionp.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cAutoDetectionp : MonoBehaviour
+{
+    public LayerMask DetectLayer;
+    public GameObject Target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // 플레이어 감지
+        if ((DetectLayer & (1 << other.gameObject.layer)) == 64)
+        {
+            if (this.transform.parent.GetComponent<cMonsterp>().myState == cMonsterp.STATE.ROAMING)
+            {
+                Target = other.gameObject; // 리스트에 넣음
+                this.transform.parent.GetComponent<cMonsterp>().OnBattle(); // 몬스터를 배틀상태로 변경
+            }
+        }
+        // 몬스터 감지
+        else if ((DetectLayer & (1 << other.gameObject.layer)) == 256)
+        {
+            if (this.transform.parent.GetComponent<cCharacter>().myState == cCharacter.STATE.PLAY)
+            {
+                Target = other.gameObject; // 리스트에 넣음
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if((DetectLayer & (1 << other.gameObject.layer)) > 0)
+        {
+            Target = null; // 타겟 해제
+        }
+    }
+}
+
+
